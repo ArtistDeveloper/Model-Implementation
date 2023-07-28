1f796e7cfefe3fd88dc8ca13a4d54dfbeffa76a3
kNU_local <somadubel@gmail.com>
2023-07-28 12:08:37 +0900

    fix: 모델 안돌아가는 코드 고침 - return out안해서 문제였던 것.

--- CNN/Resnet18/DukeClassification/model/Resnet18.py
+++ CNN/Resnet18/DukeClassification/model/Resnet18.py
@@ -16,24 +16,64 @@ class BasicBlock(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
-        self.shortcut = nn.Sequential()
-        if stride != 1:
+        self.shortcut = nn.Sequential() # identity인 경우. 비어있는 컨테이너이며, forward에서 x를 입력받아 residual 연산을 할 수 있도록 만든다.
+        if stride != 1: # stride가 1이 아니라면, identity mapping이 아닌 경우이다. 이 경우에는 projection을 수행하여 forward에서 계산될 수 있도록 만든다.
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(planes)
             )
              
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x))) 
-        out = self.bn1(self.conv2(out))
-        out += self.shortcut
+        out = F.relu(self.bn1(self.conv1(x)))
+        print("Basicblock conv1, bn1: ", out.shape)
+        out = self.bn2(self.conv2(out)) # 여기까지가 F(x)
+        print("Basicblock conv2, bn2: ", out.shape)
+        out += self.shortcut(x) # F(x) + x
         out = F.relu(out)
         return out
 
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64 # TODO 의료 이미지의 크기에 맞춰 변형예정
-        
-        # self.conv1 = nn.Conv2d
 
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=2):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        # 입력 이미지의 사이즈: 128x128
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False) # shape= 1x128x128 -> shape= 64x128x128
+        self.bn = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1) # shape= 64x128x128 -> shape= 64x128x128
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2) # shape= 64x128x128 -> shape= 128x64x64
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2) # shape= 128x64x64 -> shape= 256x32x32
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2) # shape= 256x32x32 -> shape= 512x16x16
+        self.linear = nn.Linear(512, num_classes)
+
+
+    def _make_layer(self, block, out_planes, num_block, stride):
+        strides = [stride] + ([1] * (num_block - 1))
+
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, out_planes, stride))
+            self.in_planes = out_planes
+
+        return nn.Sequential(*layers)    
+
+
+    def forward(self, x):
+        out = self.bn(self.conv1(x))
+        print("ResNet conv1, bn: ", out.shape)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 16) # input tensor=512x16x16, kernel_size=16 -> 512x1x1
+        print("after avg_pool2d shape: ", out.shape)
+        out = out.view(out.size(0), -1) # 512x1x1 -> 512x1
+        out = self.linear(out)
+        print("linear.shape: ", out.shape)
+        return out
+
+
+def ResNet18():
+    return ResNet(BasicBlock, [2, 2, 2, 2])
--- CNN/Resnet18/DukeClassification/train/train.py
+++ CNN/Resnet18/DukeClassification/train/train.py
@@ -1,11 +1,19 @@
+import os
+
 from CNN.Resnet18.DukeClassification.dataset.duke_breast_ataset import DukeDataset
-from CNN.Resnet18.DukeClassification.model.resnet18 import ResNet
+from CNN.Resnet18.DukeClassification.model.resnet18 import ResNet, ResNet18
 
 import torch
-import torchvision
-import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
 
+import torchvision
+import torchvision.transforms as transforms
+
+from torchsummary import summary
 
 def get_duke_dataloader(png_dir, train_batchsize=200, eval_batchsize = 10):
     dataset = DukeDataset(png_data_dir=png_dir, img_size=128)
@@ -39,9 +47,109 @@ def main():
     train_loader, validation_loader, test_loader = get_duke_dataloader(PNG_DIR, train_batchsize=200, eval_batchsize = 10)
     # print(len(train_loader.dataset), len(validation_loader.dataset), len(test_loader.dataset))
 
-    # net = 
+    net = ResNet18()
+    net = net.to(device)
+    # print(summary(net, input_size=(1,128,128)))
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+    learning_rate = 0.1
+    file_name = 'resnet18_cifar10.pt'
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=0.0002)
+
+    def train(epoch):
+        print('\n[ Train epoch: %d]' % epoch)
+        net.train()
+        train_loss = 0
+        correct = 0
+        total_data_num = 0
+
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            # optimizer.zero_grad()
+            for param in net.parameters():
+                param.grad = None
+
+            print("inputs type: ", type(inputs))
+            print("inputs shape: ", inputs.shape)
+            benign_outputs = net(inputs)
+            print("benign_outputs type: ", type(benign_outputs))
+            print("targets.shape: ", targets.shape)
+            loss = criterion(benign_outputs, targets)
+            loss.backward()
+            
+            optimizer.step()
+            train_loss += loss.item()
+            _, predicted_idx = benign_outputs.max(1)
+
+            total_data_num += targets.size(0) # 128
+            correct += predicted_idx.eq(targets).sum().item() # 예측이 맞은 데이터의 개수를 correct에 더함
+
+            if batch_idx % 100 == 0:
+                print('\nCurrent batch:', str(batch_idx))
+                print('Current benign train accuracy:', str(predicted_idx.eq(targets).sum().item() / targets.size(0)))
+                print('Current benign train loss:', loss.item())
+        
+        total_train_accuracy = correct / total_data_num * 100.
+        print('\nTotal benign train accuracy:', total_train_accuracy)
+        print('Total benign train loss:', train_loss)
+
+        return total_train_accuracy
 
 
+    def test(epoch):
+        print('\n[ Test epoch: %d]' % epoch)
+        net.eval()
+        loss = 0
+        correct = 0
+        total_data_num = 0
+
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            total_data_num += targets.size(0)
+
+            outputs = net(inputs)
+            loss += criterion(outputs, targets).item()
+
+            _, predicted = outputs.max(1)
+            correct += predicted.eq(targets).sum().item()
+
+
+        total_test_accuracy = correct / total_data_num * 100.
+        print('\nTest acuracy:', total_test_accuracy)
+        print('Test average loss:', loss / total_data_num)
+
+        state = {
+            'net': net.state_dict()
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/' + file_name)
+        print('Model Saved!')
+
+        return total_test_accuracy
+
+
+    def adjust_learning_rate(optimizer, epoch): # Learning rate scheduler
+        lr = learning_rate
+        if epoch >= 100:
+            lr /= 10
+        if epoch >= 150:
+            lr /= 10
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+
+    train_accuracies = []
+    test_accuracies = []
+
+    for epoch in range(0, 200):
+        adjust_learning_rate(optimizer, epoch)
+        train_accuracies.append(train(epoch))
+        test_accuracies.append(test(epoch))
+
+    
 
 
 if __name__ == '__main__':
