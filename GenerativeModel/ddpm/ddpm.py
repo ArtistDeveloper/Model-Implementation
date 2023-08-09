199de7a24fc682a0e5d75c4a1e2593d5b8588f31
kNU_local <somadubel@gmail.com>
2023-08-10 01:48:40 +0900

    refactor: 변수 이름 수정 및 주석 변경

--- GenerativeModel/ddpm/ddpm.py
+++ GenerativeModel/ddpm/ddpm.py
@@ -69,48 +69,77 @@ def load_image():
     return image
 
 
-def extract(alphas, t, x_shape):
-    batch_size = t.shape[0]
-    out = alphas.gather(-1, t.cpu())
-    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+def extract(alphas, target_t, x_shape):
+    """
+    total_timestep까지 구해놓은 alphas의 텐서와 목표하는 timestep까지의 텐서를 받아 
+
+
+    """
+    batch_size = target_t.shape[0]
+    out = alphas.gather(-1, target_t.cpu())
+    print("out: ", out)
+
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(target_t.device)
 
 
 class Diffusion(nn.Module):
     def __init__(
             self, 
-            timesteps = 300
+            total_timesteps = 300
     ):
+        """
+        아래 변수 모두 timestep만큼의 size를 가지고 있다. 학습이랑 샘플링 때 필요한 t에 맞게 index를 가져가면 된다.
+
+        # Forward process
+        alphas: 
+            ddpm (eq.4)위에 alpha의 정의가 있다. 1 - beta가 알파이다.
+
+        alphas_cumprod: 
+            ddpm (eq.4)위에 alpha_bar의 정의가 있다. 1~t까지의 누적곱을 뜻한다.
+
+        alphas_cumprod_prev: 
+            샘플링 과정(eq.6)에서 뮤 틸드를 구할 때 필요. 
+            알파 바의 t-1이 필요하기에 맨 마지막을 제거하고 맨 처음에 알파가 하나도 없었다는 뜻의 1을 집어넣는다.
+
+        # Training
+        sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod:
+                학습 과정에서 필요. DDPM의 Algorithm 1 Training 참고.
+
+        # Sampling
+        self.sqrt_recip_alphas = torch.sqrt(1.0 / self.alphas):
+            루트 알파 바의 역수가 샘플링 과정에서 필요하다. 
+        
+
+        """
         # beta schedule 정의
-        self.betas = ForwardBetaSchedule.linear_beta_schedule(timesteps=timesteps)
+        self.betas = ForwardBetaSchedule.linear_beta_schedule(timesteps=total_timesteps)
 
-        # alphas 정의 
-        self.alphas = 1. - self.betas # alpha: 1 - beta
-        self.alphas_cumprod = torch.cumprod(self.alphas, axis=0) # alpha bar: start ~ timestep까지의 누적곱
-        self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1, 0), value=1.0) # 마지막 숫자 제외한 뒤 맨 앞(왼쪽)에 값 1.0으로 한 개의 패딩 추가
+        self.alphas = 1. - self.betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, axis=0)
+        self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1, 0), value=1.0) 
+
+        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod) 
+        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod) 
+
         self.sqrt_recip_alphas = torch.sqrt(1.0 / self.alphas)
-
-        # (ddpm eq.4) diffusion q(x_t | x_{t-1}) 및 다른 것들 계산
-        # 이 방식으로 샘플링을 할 때, 한 번에 노이즈를 다 입힌 이미지로 변환가능하다. 
-        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod) # 정규분포의 평균에서 사용할 값
-        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod) # 정규분포의 표준편차에서 사용할 값
 
         # 사후 확률 q(x_{t-1} | x_t, x_0)에 대한 계산
         self.posterior_variance = self.betas * (1. - self.alphas_cumprod_prev) / (1. - self.alphas_cumprod)
 
     
-    # 포워드 디퓨전 (using the nice property)
-    def q_sample(self, x_start:torch.Tensor, t:torch.Tensor, noise=None):
+    # forward diffusion. 목표하는 타입스텝까지의 tensor들을 extract해서 텐서를 가져온다.
+    def q_sample(self, x_start:torch.Tensor, target_t:torch.Tensor, noise=None):
         if noise is None:
             noise = torch.randn_like(x_start)
 
-        sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, t, x_start.shape)
-        sqrt_one_minus_alphas_cumprod_t = extract(self.sqrt_one_minus_alphas_cumprod, t, x_start.shape)
+        sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, target_t, x_start.shape) 
+        sqrt_one_minus_alphas_cumprod_t = extract(self.sqrt_one_minus_alphas_cumprod, target_t, x_start.shape)
 
-        return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
+        return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise # 샘플링 수식인가?
     
 
-    def get_noisy_image(self, x_start, t):
-        x_noisy = self.q_sample(x_start, t=t)
+    def get_noisy_image(self, x_start, target_t):
+        x_noisy = self.q_sample(x_start, target_t)
         
         # 역변환을 정의하여, [-1, 1]의 값을 포함하는 PyTorch 텐서를 받아 이를 다시 PIL 이미지로 변환
         reverse_transform = Compose([
@@ -137,13 +166,16 @@ class Diffusion(nn.Module):
             Lambda(lambda t: (t * 2) - 1), # [-1, 1] 사이로 값 정규화
         ])
 
-        # preprocessing image, it is start of x
-        x_start = transform(image).unsqueeze(0) # add the '1' dimension for 0 dim index. [3,128,128] -> [1,3,128,128]
+        # 이미지 전처리 후 '0'dim 인덱스에 '1'인 차원을 추가한다. ex) [3,128,128] -> [1,3,128,128]        
+        # 원본 이미지
+        x_start = transform(image).unsqueeze(0) 
 
         # take time step
         t = torch.tensor([40])
 
+        # get noisy_image
         noisy_image = self.get_noisy_image(x_start, t)
+        
         print(type(noisy_image))
         plt.imshow(noisy_image)
         plt.show()
@@ -153,6 +185,6 @@ class Diffusion(nn.Module):
 
 # 정리된 main
 if __name__ == '__main__':
-    model = Diffusion(timesteps=300)
+    model = Diffusion(total_timesteps=300)
     model.test_forward_process()
 
