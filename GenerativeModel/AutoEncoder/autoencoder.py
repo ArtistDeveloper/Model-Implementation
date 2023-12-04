0d52df641831704de9e94646d1cc7582784121e7
server <somadubel@gmail.com>
2023-12-04 16:16:02 +0000

    feat: autoencoder cnn 구현(1)

--- GenerativeModel/AutoEncoder/autoencoder.py
+++ GenerativeModel/AutoEncoder/autoencoder.py
@@ -50,12 +50,31 @@ class AutoEncoderConv(nn.Module):
         super(AutoEncoderConv, self).__init__()
         
         self.encoder = nn.Sequential(
+            # 28x28 -> 14x14
+            nn.Conv2d(1, 64, 3, stride=1, padding=1),
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
+            nn.ReLU(),
             
+            # 14x14 -> 7x7
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.Conv2d(128, 256, 3, stride=2, padding=1),
+            nn.ReLU(),
+            
+            # 7x7 -> 3x3
+            nn.Conv2d(256, 256, 3, stride=1, padding=1),
+            nn.Conv2d(256, 512, 4, stride=2, padding=1),
+            nn.ReLU(),
+        )
+        
+        self.decoder = nn.Sequential(
+            # nn.ConvTranspose2d(512, 256, 3, )
         )
         
     
     def forward(self, x):
-        return NotImplemented
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return encoded
 
 
 def visualize_latent_variable(autoencoder, trainset, device):
